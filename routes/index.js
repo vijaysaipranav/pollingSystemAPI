0d7dbda7b1e0f0b249ff1ec1f17d25f8415e6ee9
vijaysaipranav <vijaysaiprant@gmail.com>
2023-07-19 23:37:52 +0530

    new updates

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 console.log('Router Loaded');
 // implementing questions and options routes
+router.get('/',async(req,res)=>{
+    res.redirect('/questions/create')
+})
 router.use('/questions',require('./questions'));
 router.use('/options',require('./options'));
 module.exports = router;
